60160b185851b62e8cde67c1dab01bcab09c61a8
saidazimov720 <saidazimovnurbek01@gmail.com>
2024-10-06 17:34:30 +0500

    add the js

--- pages/home/js/index.js
+++ pages/home/js/index.js
@@ -4,3 +4,15 @@ likeableIcons.forEach(icon => {
         this.classList.toggle('liked'); 
     });
 });
+
+document.addEventListener('DOMContentLoaded', function () {
+    // Находим бургер-иконку и меню
+    const menuIcon = document.querySelector('.menu-icon');
+    const mobileMenu = document.getElementById('mobile-menu');
+
+    // Добавляем обработчик событий на бургер-иконку
+    menuIcon.addEventListener('click', function () {
+        mobileMenu.classList.toggle('active'); // Переключаем класс active на меню
+    });
+});
+
